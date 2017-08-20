eac36c770e2ca479437a17a8a41dc0b8ab64ba09
lichunlong <licl@primeton.com>
2017-08-20 15:58:33 +0800

    env

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: '"development"',
-    BASE_API: '"https://api-dev"',
+    BASE_API: '"http://127.0.0.1:8000/stock"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 }
