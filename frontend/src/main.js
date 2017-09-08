c233ce1544c31e231a3e5cd345bab1b1737e8619
lichunlong <licl@primeton.com>
2017-09-08 10:09:36 +0800

    ie11 兼容

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import router from './router';
 import store from './store';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-default/index.css';
+import 'es6-promise/auto';
 import 'assets/custom-theme/index.css'; // 换肤版本element-ui css
 import NProgress from 'nprogress'; // Progress 进度条
 import 'nprogress/nprogress.css';// Progress 进度条 样式
