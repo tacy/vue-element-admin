0de7ebbdef6d9f6e930e3c735c2680ffc36ab2d2
lichunlong <licl@primeton.com>
2017-10-18 10:15:16 +0800

    更新库位号需要查商品表, 确认商品信息是否在系统

--- ymatou/csv-import.py
+++ ymatou/csv-import.py
@@ -15,6 +15,8 @@ with open('./kuwei.csv', newline='') as f:
     location = ''
     sql = 'update stock_stock s inner join stock_product p on s.product_id=p.id set location=%s where p.jancode=%s and s.inventory_id=4'
     query_sql = 'select p.jancode, s.location from stock_stock s inner join stock_product p on s.product_id=p.id where s.inventory_id=4 and p.jancode=%s'
+    product_sql = 'select id,jancode from stock_product where jancode=%s'
+    insert_sql = 'insert into stock_stock (quantity, inflight, preallocation, inventory_id,product_id) values(0,0,0,4,%s)'
 
     try:
         with connection.cursor() as cursor:
@@ -26,8 +28,14 @@ with open('./kuwei.csv', newline='') as f:
                 cursor.execute(query_sql, (row[0]))
                 result = cursor.fetchall()
                 if not result:
-                    print('jancode does not exist: %s' % (row[0]))
-                    continue
+                    cursor.execute(product_sql, (row[0]))
+                    result = cursor.fetchall()
+                    if not result:
+                        print('jancode does not exist: %s' % (row[0]))
+                        continue
+                    else:
+                        cursor.execute(insert_sql, (result[0]['id']))
+                        result[0]['location'] = None
                 db_location = result[0]['location']
                 if db_location and location not in db_location:
                     db_location = db_location + ',' + location
