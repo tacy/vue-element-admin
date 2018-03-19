4ca87b95464a5017e5d04da7b5cdd33329693a5a
lichunlong <licl@primeton.com>
2018-03-19 23:38:05 +0800

    Fixbug: 前一个提交引入的bug

--- stock/views/xlobo.py
+++ stock/views/xlobo.py
@@ -620,7 +620,7 @@ def getInvoiceInfo(db_type, result):
         'shipping':
         'select p.name, p.specification, o.jancode, o.quantity, s.location, o.seller_memo, o.real_price*o.quantity pay from stock_order o inner join stock_product p on o.jancode=p.jancode inner join stock_stock s on s.product_id=p.id where o.shippingdb_id=%s and s.inventory_id=%s',
         'transform':
-        'select p.name,p.specification, p.jancode, poi.quantity,s.location,"",o.real_price*o.quantity pay from stock_purchaseorderitem poi inner join stock_product p on poi.product_id=p.id inner join stock_stock s on s.product_id=p.id where poi.status="转运中" and poi.transformdb_id=%s and s.inventory_id=%s',
+        'select p.name,p.specification, p.jancode, poi.quantity,s.location,"","" pay from stock_purchaseorderitem poi inner join stock_product p on poi.product_id=p.id inner join stock_stock s on s.product_id=p.id where poi.status="转运中" and poi.transformdb_id=%s and s.inventory_id=%s',
     }
     ptype = 'xlobo' if 'DB' in result[0]['BillCode'].upper() else 'ems'
     for i, b in enumerate(result):
